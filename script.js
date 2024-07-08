bac88b568cf98779f3eba458ad3c570fb397a8b0
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-07 23:53:13 -0500

    Edit change grid size button to only accept whole numbers

--- script.js
+++ script.js
@@ -16,14 +16,16 @@ let gridSizeButton = document.querySelector('.grid-size-button');
 gridSizeButton.addEventListener('click', () => {
     let newGridSizePrompt = prompt('Enter a whole number between 10-100 to change the number of squares per row.');
     let newGridSize = Number(newGridSizePrompt.slice(0));
-    console.log(newGridSizePrompt);
-    console.log(typeof(newGridSize));
+    console.log(newGridSize);
+    console.log(parseInt(newGridSize));
     if ((newGridSizePrompt === '') || isNaN(newGridSize)) {
         alert('Invalid grid size: please enter a number.')
+    } else if (newGridSize !== parseInt(newGridSize)) {
+        alert('Invalid grid size: please enter a whole number.')
     } else if (newGridSize < 10) {
-        alert('Invalid grid size: number cannot be less than 10.')
+        alert('Invalid grid size: number must be greater than or equal to 10.')
     } else if (newGridSize > 100) {
-        alert('Invalid grid size: number cannot be greater than 100.')
+        alert('Invalid grid size: number must be less than or equal to 100.')
     } else {
         while (gridContainer.firstChild) {
             gridContainer.removeChild(gridContainer.firstChild);
