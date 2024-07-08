9a4c6c93928a82c2e9de647d2bddb6c0a1495446
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-07 22:56:30 -0500

    Add button to change grid size
    Added button that wipes all existing grid squares and
    then prompts user to enter a new number of grid squares
    per row before generating a new grid with said number of
    squares.

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 let gridContainer = document.querySelector('.grid-container');
-for (i = 1; i <= 256; i++) {
+for (i = 1; i <= (16 ** 2); i++) {
     let gridSquare = document.createElement('div');
     gridSquare.classList.add(`grid-square`);
     gridSquare.classList.add(`square${i}`);
+    let squareSize = (600 / 16);
+    gridSquare.style.width = `${squareSize}px`;
+    gridSquare.style.height = `${squareSize}px`;
     gridContainer.appendChild(gridSquare);
     gridSquare.addEventListener('mouseover', () => {
         gridSquare.style.backgroundColor = 'black';
     });
 }
+
+let gridSizeButton = document.querySelector('.grid-size-button');
+gridSizeButton.addEventListener('click', () => {
+    let newGridSize = Number(prompt('Enter a whole number between 10-100 to change the number of squares per row.'));
+    console.log(newGridSize);
+    console.log(typeof(newGridSize));
+    if (typeof (newGridSize) !== 'number') {
+        alert('Invalid grid size: only numbers are accepted.')
+    } else if (newGridSize < 10) {
+        alert('Invalid grid size: number cannot be less than 10.')
+    } else if (newGridSize > 100) {
+        alert('Invalid grid size: number cannot be greater than 100.')
+    } else {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        };
+        for (i = 1; i <= (newGridSize ** 2); i++) {
+            let gridSquare = document.createElement('div');
+            gridSquare.classList.add(`grid-square`);
+            gridSquare.classList.add(`square${i}`);
+            let squareSize = (600 / newGridSize);
+            gridSquare.style.width = `${squareSize}px`;
+            gridSquare.style.height = `${squareSize}px`;
+            gridContainer.appendChild(gridSquare);
+            gridSquare.addEventListener('mouseover', () => {
+                gridSquare.style.backgroundColor = 'black';
+            });
+        }
+    }
+});
