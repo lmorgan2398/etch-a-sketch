60717cc9fe044242922ba16ec058b4870a674319
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-07 21:45:22 -0500

    Create 16x16 grid of square divs
    Created a grid container in HTML to house the invdividual divs
    used to make the grid, then used JS to iterate through a loop
    creating a square div for 16^2 (256) iterations, leaving a 16x16
    grid. Styled the grid and the container to have a border.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('working');
+let gridContainer = document.querySelector('.grid-container');
+for (i = 1; i <= 256; i++) {
+    let gridSquare = document.createElement('div');
+    gridSquare.classList.add(`grid-square`);
+    gridSquare.classList.add(`square${i}`);
+    gridContainer.appendChild(gridSquare);
+}
